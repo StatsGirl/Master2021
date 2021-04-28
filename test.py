aa5c743fd5585876cba935c1257c7c72e158be6f
StatsGirl <breyawalker@gmail.com>
2021-04-28 01:18:43 -0500

    test.py file showing import of package and functions/how to use

---
+++ test.py
@@ -0,0 +1,11 @@
+from combinationpvalues import InfinitePs
+from combinationpvalues import CountPs
+from combinationpvalues import StoufferMethod
+
+import combinationpvalues as cp
+
+cp.InfinitePs()
+cp.CountPs()
+cp.StoufferMethod()
+
+
